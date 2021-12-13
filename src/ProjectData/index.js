93d5d1fcf9b8c1b7e3a7b322a72b8aef0e68a6b1
Leslie Codes <84750526+Lesliescodes@users.noreply.github.com>
2021-12-13 01:38:15 -0700

    added files

---
+++ src/ProjectData/index.js
@@ -0,0 +1,26 @@
+//import  from '../assets/images/gamego.png';
+
+
+const data = [
+    {
+        name: 'Budget Tracker',
+        github: 'https://github.com/Lesliescodes/budgetTracker',
+        deploy: '',
+        photo: ""
+    },
+
+    {
+        name: 'Note Taker',
+        github: 'https://github.com/Lesliescodes/note-taker',
+        deploy: 'https://gentle-cliffs-78871.herokuapp.com/',
+        photo: ""
+    },
+    {
+        name: 'Code Quiz',
+        github: 'https://github.com/Lesliescodes/code-quiz',
+        deploy: 'https://lesliescodes.github.io/code-quiz/',
+        photo: ""
+    },
+    
+]
+    export default data;
